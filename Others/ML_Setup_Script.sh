5d572328b889f4d084904e1c0de912a42d868ab3
Satyam5878 <guptasatyam5878@gmail.com>
2018-02-24 23:19:57 +0530

    C: Added the 'Others' Folder and Links file.

---
+++ Others/ML_Setup_Script.sh
@@ -0,0 +1,16 @@
+# installing numpy
+sudo pip3 install numpy
+# installing scipy
+pip3 install scipy
+# installing matplotlib
+pip install matplotlib
+# installing pandas
+pip3 install pandas
+# installing sklearn
+pip3 install sklearn
+# installing tensorflow
+pip3 install tensorflow
+
+
+# for installing tkinter to show the graph in matplotlib
+sudo apt-get install tcl-dev tk-dev python-tk python3-tk
